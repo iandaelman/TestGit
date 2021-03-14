b84faffc56c7e3397edac68d86b7f824109c8df2
Ian Daelman <ian.daelman@student.hogent.be>
2021-03-14 14:49:31 +0100

    Toevoegen constructor en getten/setter

--- src/domain/Test.java
+++ src/domain/Test.java
@@ -2,4 +2,22 @@ package domain;
 
 public class Test {
 
+	int getal;
+
+	public Test(int getal) {
+		super();
+		this.getal = getal;
+	}
+
+	public int getGetal() {
+		return getal;
+	}
+
+	public void setGetal(int getal) {
+		this.getal = getal;
+	}
+	
+	
+	
+	
 }
