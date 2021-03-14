30fa0decb9782169aaedb2282786a8e2b1c8a10b
Ian Daelman <ian.daelman@student.hogent.be>
2021-03-14 14:55:38 +0100

    attribuut toegegoegd en andere zaken

--- src/domain/Test.java
+++ src/domain/Test.java
@@ -2,20 +2,27 @@ package domain;
 
 public class Test {
 
-	int getal;
-
-	public Test(int getal) {
+	int getal1;
+	int getal2;
+	
+	public Test(int getal1, int getal2) {
 		super();
-		this.getal = getal;
+		this.getal1 = getal1;
+		this.getal2 = getal2;
+	}
+	public int getGetal1() {
+		return getal1;
+	}
+	public void setGetal1(int getal1) {
+		this.getal1 = getal1;
+	}
+	public int getGetal2() {
+		return getal2;
+	}
+	public void setGetal2(int getal2) {
+		this.getal2 = getal2;
 	}
 
-	public int getGetal() {
-		return getal;
-	}
-
-	public void setGetal(int getal) {
-		this.getal = getal;
-	}
 	
 	
 	
